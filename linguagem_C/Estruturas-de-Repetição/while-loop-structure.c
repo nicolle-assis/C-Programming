66d9b45385d550573711b8c287de3672f1f98565
Nicolle de Assis <nicolledeassis13@gmail.com>
2025-11-02 20:49:35 -0300

    Create while-loop-structure.c

---
+++ linguagem_C/Estruturas-de-Repetição/while-loop-structure.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[30];
+    int i;
+
+    printf("Informe o nome: ");
+    scanf("%s", name);
+
+    i=0;
+    while(i!=10)
+    {
+        printf("\n %d - %s", i, name); i++;
+    }
+
+    return 0;
+}
